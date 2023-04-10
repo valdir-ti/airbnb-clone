27f9b3b5e42db4942aad45d44d3f00a7cf8025f4
Valdir Silva <valdir.ti@gmail.com>
2023-04-09 23:13:12 -0300

    feat: add register mongodb funcionality

---
+++ app/api/register/route.ts
@@ -0,0 +1,19 @@
+import bcrypt from 'bcrypt'
+import { NextResponse } from 'next/server'
+
+import prisma from '@/app/libs/prismadb'
+
+export async function POST(req: Request){
+
+    const body = await req.json()
+
+    const { email, name, password } = body
+
+    const hashedPassword = await bcrypt.hash(password, 12)
+
+    const user = await prisma.user.create({
+        data: { email, name, hashedPassword }
+    })
+
+    return NextResponse.json(user)
+}
